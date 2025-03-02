85962c79faf98bd67f37560b4b4712351a257617
mirzamar <132921031+mirzamar@users.noreply.github.com>
2025-03-02 17:04:50 +0100

    nav komponente hinzugefuegt

--- src/app/transaktionen/transaktionen.component.ts
+++ src/app/transaktionen/transaktionen.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-transaktionen',
   imports: [],
+  standalone: true,
   templateUrl: './transaktionen.component.html',
   styleUrl: './transaktionen.component.css'
 })
