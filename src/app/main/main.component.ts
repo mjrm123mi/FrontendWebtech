54480ab8f1b973245a3c5d98c4f9f1210c8bd3eb
mirzamar <132921031+mirzamar@users.noreply.github.com>
2025-03-03 21:04:24 +0100

    Dialog gefixt

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,17 +1,32 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TransactionDialogComponent } from '../transaction-dialog/transaction-dialog.component';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton
+  ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
 
-  addTransaction() {
-    console.log("Button geklickt")
+  constructor(private dialog: MatDialog) { }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(TransactionDialogComponent, {
+      width: '500px',
+      disableClose: true
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        // Hier kannst du die Daten ans Backend senden
+        console.log("Main-Component: " + result);
+      }
+    });
   }
 }
--- src/app/transaction-dialog/transaction-dialog.component.ts
+++ src/app/transaction-dialog/transaction-dialog.component.ts
@@ -1,26 +1,66 @@
 import { Component, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogClose, MatDialogActions
+} from '@angular/material/dialog';
+import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import {MatFormField, MatFormFieldModule} from '@angular/material/form-field';
+import {MatInput} from '@angular/material/input';
+import {MatOption, MatSelect} from '@angular/material/select';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {MatButton} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-transaction-dialog',
   templateUrl: './transaction-dialog.component.html',
   standalone: true,
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormField,
+    MatFormFieldModule,
+    MatInput,
+    MatSelect,
+    MatOption,
+    MatDialogClose,
+    MatDialogActions,
+    MatButton
+  ],
   styleUrls: ['./transaction-dialog.component.css']
 })
 export class TransactionDialogComponent {
 
+  transactionForm: FormGroup;
+
   constructor(
     public dialogRef: MatDialogRef<TransactionDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
-  ) { }
+  ) {
+    this.transactionForm = new FormGroup({
+      date: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      amount: new FormControl('', Validators.required),
+      category: new FormControl('', Validators.required)
+    });
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
   }
 
-  onSubmit(transactionData: any): void {
+  onSubmit(): void {
     // Hier können die Daten ans Backend gesendet werden
-    console.log(transactionData);
-    this.dialogRef.close(transactionData);
+    if (this.transactionForm.valid) {
+      console.log("Dialog-Component: " + this.transactionForm.value);
+      this.dialogRef.close(this.transactionForm.value);
+    } else {
+      console.log('Formular ist nicht gültig');
+    }
   }
 }
