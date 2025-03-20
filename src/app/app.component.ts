7f0ab3392c9a2b97116b7cdc944e264738e4fba4
Maryam Mirza <176325459+mjrm123mi@users.noreply.github.com>
2025-03-20 14:08:01 +0100

    Footer hinzugefuegt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { MainComponent } from './main/main.component';
 import { TransaktionenComponent } from './transaktionen/transaktionen.component';
+import {FooterComponent} from './footer/footer.component';
 
 // app.component.ts und die anderen app.component Dateien sind die Hauptanwendung und die importiert alle anderen Komponenten.
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, MainComponent, TransaktionenComponent],
+  imports: [RouterOutlet, HeaderComponent, MainComponent, TransaktionenComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
